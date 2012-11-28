68c421c443e93caf54125ca7dd74b8c745d7f9fc
Rikhil Jain <rikhiljain@gmail.com>
2012-11-28 21:28:17 +0530

    Inital Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nikrik::Application.config.secret_token = 'be1e7d5ba0b0ae20f9d44cce8a86d7cfb2b9de5dcc403f0f301062bc95b9136508db8ff4c68a154e51c0e09d9ff7afb9bf07733fd76670142d3dbe678f12acdb'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Nikrik::Application.routes.draw do
+
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users
+  resources :users
+end
