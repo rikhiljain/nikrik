0d1d042c6b826df4992f26be5d016fa0ba5ea2dc
Nikhil P Agrawal <nikhil.pa@gmail.com>
2012-12-10 23:20:11 +0530

    welcome page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,60 @@
-    $(function() {
-        $( "#tabs" ).tabs();
-    });
+(function($){
+	$(document).ready(function(){
+		$( "#tabs" ).tabs();
+		populateManufacturers();
+		pouplateDayForRegistrationDetails();
+		pouplateMonthForRegistrationDetails();
+		pouplateYearForRegistrationDetails();
+	});
+})(jQuery);	
+
+function populateManufacturers(){
+	var options;
+	$.getJSON('make.json',function(options){
+		$.each(options, function(){
+			options += '<option value="' + this['optionValue'] + '">' + this['optionDisplay'] + '</option>';
+		});
+		$("#dynamic_slct").html(options);
+	});
+}
+
+function pouplateDayForRegistrationDetails(){
+	var options; 
+	for (var i=1; i <= 31;++i) { 
+		options += '<option value="' + i + '">' + i + '</option>';
+	 }
+	$("#day").html(options);
+}
+
+function pouplateMonthForRegistrationDetails(){
+	var options = [
+	               	{"optionValue":"01", "optionDisplay": "Jan"},
+	               	{"optionValue":"02", "optionDisplay": "Feb"},
+	               	{"optionValue":"03", "optionDisplay": "Mar"},
+	               	{"optionValue":"04", "optionDisplay": "Apr"},
+	               	{"optionValue":"05", "optionDisplay": "May"},
+	               	{"optionValue":"06", "optionDisplay": "Jun"},
+	               	{"optionValue":"07", "optionDisplay": "Jul"},
+	               	{"optionValue":"08", "optionDisplay": "Aug"},
+	               	{"optionValue":"09", "optionDisplay": "Sep"},
+	               	{"optionValue":"10", "optionDisplay": "Oct"},
+	               	{"optionValue":"11", "optionDisplay": "Nov"},
+	               	{"optionValue":"12", "optionDisplay": "Dec"}
+	              ];
+	$.each(options, function(){
+		options += '<option value="' + this['optionValue'] + '">' + this['optionDisplay'] + '</option>';
+	});
+
+	$("#month").html(options);
+	
+}
+
+function pouplateYearForRegistrationDetails(){
+	var options; 
+	for (var i=1999; i <= 2020;++i) { 
+		options += '<option value="' + i + '">' + i + '</option>';
+	 }
+	$("#year").html(options);
+	
+}
+  
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-  	@motorquote = Motorquote.new()
+  	
   end
 end
