29b9b9f24a6c03729907f011c286aa2f1f55f8a4
Nikhil P Agrawal <nikhil.pa@gmail.com>
2012-12-12 22:55:39 +0530

    selector logic in jquery made consistent

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -26,12 +26,11 @@ function populateManufacturers(){
 		$.each(options, function(){
 			options += '<option value="' + this['optionValue'] + '">' + this['optionDisplay'] + '</option>';
 		});
-		$("#dynamic_make").html(options);
+		$("[id=basicDetails] > [id=vehicleDetails] > [id=make]").html(options);
 	});
 }
 
 function populateModel(manufacturer){
-	//alert(manufacturer);
 	var options;
 	var address = "http://localhost:3000/idv_charts/modelsForAManufacturer.json?manufacturer="+manufacturer;
 	//var address = "make.json";
@@ -39,7 +38,7 @@ function populateModel(manufacturer){
 		$.each(options, function(){
 			options += '<option value="' + this['optionValue'] + '">' + this['optionDisplay'] + '</option>';
 		});
-		$("#dynamic_model").html(options);
+		$("[id=basicDetails] > [id=vehicleDetails] > [id=model]").html(options);
 	});
 	
 }
@@ -49,7 +48,7 @@ function pouplateDayForRegistrationDetails(){
 	for (var i=1; i <= 31;++i) { 
 		options += '<option value="' + i + '">' + i + '</option>';
 	 }
-	$("#day").html(options);
+	$("[id=basicDetails] > [id=registrationDetails] > [id=day]").html(options);
 }
 
 function pouplateMonthForRegistrationDetails(){
@@ -71,7 +70,7 @@ function pouplateMonthForRegistrationDetails(){
 		options += '<option value="' + this['optionValue'] + '">' + this['optionDisplay'] + '</option>';
 	});
 
-	$("#month").html(options);
+	$("[id=basicDetails] > [id=registrationDetails] > [id=month]").html(options);
 	
 }
 
@@ -80,13 +79,13 @@ function pouplateYearForRegistrationDetails(){
 	for (var i=1999; i <= 2020;++i) { 
 		options += '<option value="' + i + '">' + i + '</option>';
 	 }
-	$("#year").html(options);
+	$("[id=basicDetails] > [id=registrationDetails] > [id=year]").html(options);
 	
 }
 
 function bindAllEventHandlers(){
-	$("#dynamic_make").change(function(){
-		populateModel($("#dynamic_make").val());
+	$("[id=basicDetails] > [id=vehicleDetails] > [id=make]").change(function(){
+		populateModel($("[id=basicDetails] > [id=vehicleDetails] > [id=make]").val());
 	})
 }
 
