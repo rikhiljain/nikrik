92c454af861191ec7c4c170051337886048c5b01
Nikhil P Agrawal <nikhil.pa@gmail.com>
2012-12-24 23:32:27 +0530

    added tooltips

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -400,31 +400,27 @@ function validateForm(){
 
 
 function bindToolTips(){
-	//alert("hi");
-	//$('[rel=tooltip]').tooltip();
-	//$("[id=basicDetails] > [id=currentInsuranceDetails] [id=policyType_icon]").data('tooltip').options.placement = 'right';
-	//$("[id=basicDetails] > [id=currentInsuranceDetails] [id=policyType_icon]").attr('data-original-title', item.value + ' selected.').tooltip('fixTitle');
-	//$("[id=basicDetails] > [id=currentInsuranceDetails] [id=policyType_icon]").tooltip('hide').attr('data-original-title', newValue).tooltip('fixTitle').tooltip('show');
-	//$("[id=basicDetails] > [id=currentInsuranceDetails] [id=policyType_icon]").tooltip("show");
-	// $("[id=basicDetails] > [id=registrationDetails] [id=city_icon]");
-	// $("[id=basicDetails] > [id=registrationDetails] [id=day_icon]")
-	// $("[id=basicDetails] > [id=registrationDetails] [id=registrationType_icon]")
-	// $("[id=basicDetails] > [id=vehicleDetails] [id=make_icon]")	
-	// $("[id=basicDetails] > [id=vehicleDetails] [id=price_icon]")	
 
-	// $("[id=previousPolicyDetails] [id=prevPolicyType_icon]")
-	// $("[id=previousPolicyDetails] [id=date_icon]")
-	// $("[id=previousPolicyDetails] [id=newPolicyStartDate_icon]")
-	// $("[id=previousPolicyDetails] [id=claimsMade_icon]")
-	// $("[id=previousPolicyDetails] [id=ncb_icon]")
+	//$("[id=basicDetails] > [id=currentInsuranceDetails] [id=policyType_icon]").attr("title","???");
+	$("[id=basicDetails] > [id=registrationDetails] [id=city_icon]").attr("title","Please eneter first 3 letters of the RTO city");
+	$("[id=basicDetails] > [id=registrationDetails] [id=day_icon]").attr("title","Available in your vehicle's RC book");
+	$("[id=basicDetails] > [id=registrationDetails] [id=registrationType_icon]").attr("title","Select 'Individual' in case vehicle is registered in the name of an Individual");
+	$("[id=basicDetails] > [id=vehicleDetails] [id=make_icon]").attr("title","Please select the vehicle manufacturer and vehicle model");
+	$("[id=basicDetails] > [id=vehicleDetails] [id=price_icon]").attr("title","???");
 
-	// $("[id=protectionForAccessories] [id=elecAccessories_icon]")
-	// $("[id=protectionForAccessories] [id=nonElecAccessories_icon]")
-	// $("[id=protectionForAccessories] [id=kit_icon]")
-	// $("[id=protectionForAccessories] [id=kitPrice_icon]")
+	$("[id=previousPolicyDetails] [id=prevPolicyType_icon]").attr("title","Available in your previous policy");
+	//$("[id=previousPolicyDetails] [id=date_icon]").attr("title","");
+	//$("[id=previousPolicyDetails] [id=newPolicyStartDate_icon]").attr("title","");
+	//$("[id=previousPolicyDetails] [id=claimsMade_icon]").attr("title","");
+	$("[id=previousPolicyDetails] [id=ncb_icon]").attr("title","Available in your previous policy");
 
-	// $("[id=additionalDiscount] [id=antiTheft_icon]")
-	// $("[id=additionalDiscount] [id=automobileAssoc_icon]")
+	$("[id=protectionForAccessories] [id=elecAccessories_icon]").attr("title","Provides cover for electrical accessories that are not factory fitted with the vehicle");
+	$("[id=protectionForAccessories] [id=nonElecAccessories_icon]").attr("title","Provides cover for Non-electrical/Non-electronic equipments that are not factory fitted with the vehicle");
+	$("[id=protectionForAccessories] [id=kit_icon]").attr("title","Provides cover for CNG/LPG kit that is not factory fitted with the vehicle");
+	$("[id=protectionForAccessories] [id=kitPrice_icon]").attr("title","???");
+
+	$("[id=additionalDiscount] [id=antiTheft_icon]").attr("title","Get a discount if you have installed an ARAI approved anti-theft device in your vehicle");
+	$("[id=additionalDiscount] [id=automobileAssoc_icon]").attr("title","Get a discount if you are a memeber of any automobile association e.g. Western India Automobile Association");
 
 
 }
