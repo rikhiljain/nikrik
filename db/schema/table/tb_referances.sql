3ff8776fa02b04c1725dce568c3c0ffe15f974b1
Rikhil Jain <rikhiljain@gmail.com>
2013-01-31 23:27:51 +0530

    update

---
+++ db/schema/table/tb_referances.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `referances` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `mobile` varchar(10) DEFAULT NULL,
+  `ref_name` varchar(30) DEFAULT NULL,
+  `ref_mobile` varchar(10) DEFAULT NULL,
+  `ref_desc` varchar(1000) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+)  ;
---
+++ db/schema/table/tb_rewards.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `rewards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `ref_id` int(11) DEFAULT NULL,
+  `points` int(11) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `exp_dt` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
