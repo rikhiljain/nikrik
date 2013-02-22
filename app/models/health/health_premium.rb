74f71b74fb543b46ca7c69681a700e1fdf2e6352
Rikhil Jain <rikhiljain@gmail.com>
2013-02-22 23:13:21 +0530

    company name in helth

--- app/models/health/health_premium.rb
+++ app/models/health/health_premium.rb
@@ -1,4 +1,4 @@
-class Health::HealthPremium 
+class Health::HealthPremium < Motor::Premium
 
 def initialize( search)
       @input = search
@@ -7,21 +7,24 @@ def initialize( search)
   def  calculate_premium
   	results = Array.new
     health_charts = Health::HealthChart.find_by_coverage_age(@input.heath_cover, @input.adult_age)
-    companies = Company.all
-    debugger
+  
+    company_hash = {}
+    Company.all.each do |company|
+      company_hash[company.id] = company.name
+    end
 
     health_charts.each do |chart|
 
     	health_quote = Health::Quote.new
     	health_quote.company_id = chart.company_id
-      health_quote.company_name = "abc"
+      health_quote.company_name = company_hash[chart.company_id]
     	health_quote.total_premium = chart.premium
     	if(@input.policy_for == 3)
     		health_quote.total_premium = health_quote.total_premium + chart.premium * 0.5
     	end
     	health_quote.total_premium = health_quote.total_premium + @input.no_of_childs* chart.premium * 0.25
-
-      health_quote.final_premium = health_quote.total_premium
+      health_quote.service_tax = calculate_service_tax(health_quote.total_premium)
+      health_quote.final_premium = health_quote.total_premium + health_quote.service_tax
     	
     	Health::Quote.format_fields(health_quote)
      	Rails.logger.info "Final Premium= #{chart.premium}"
