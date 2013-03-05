d475db8a9c993788d2777b507ee2249c047648cc
Rikhil Jain <rikhiljain@gmail.com>
2013-03-05 08:55:09 +0530

    motor discount page change

--- db/schema/table/tb_motor_discounts.sql
+++ db/schema/table/tb_motor_discounts.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `motor_discounts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `idv_chart_id` int(11) DEFAULT NULL,
   `company_id` smallint(6) DEFAULT NULL,
-  `rto_id` int(11) NOT NULL,
+  `rto_id` int(11) DEFAULT NULL,
   `amount` float DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
