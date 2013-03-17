2a74156523f7a639f35ebfdf86849f674f9e5d1f
Nikhil P Agrawal <nikhil.pa@gmail.com>
2013-03-17 11:40:25 +0530

    table for policy compare

---
+++ db/data/policy_attributes_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `policy_attributes` (`company_id`, `policy_type_id`, `attrib_name`, `attrib_value`) VALUES
+(1, 1, 'first', 'value'),
+(1, 1, 'second', 'true'),
+(1, 1, 'third', '10'),
+(2, 1, 'first', 'value1'),
+(2, 1, 'second', 'false'),
+(2, 1, 'third', '20');
