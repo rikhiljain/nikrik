85d80fe369fc437b56c747581d5ec34ee07645b8
Nikhil P Agrawal <nikhil.pa@gmail.com>
2013-03-17 17:59:21 +0530

    improving policy compare

--- app/assets/javascripts/welcome/policy-compare.js
+++ app/assets/javascripts/welcome/policy-compare.js
@@ -3,24 +3,49 @@ function __common__policyCompare(policyType){
 	//make one ajax call to pull the data
 	//for a policy type.
 	//Polict type will be either "Motor", "Health" or "Travel"
+
+	// var policyAttributes = {
+	// 				"first":{
+	// 							"1":"value1",
+	// 							"2":"value2"
+	// 				},
+	// 				"second":{
+	// 							"1":"true",
+	// 							"2":"false"
+	// 				},
+	// 				"third":{
+	// 							"1":"10",
+	// 							"2":"20"						
+	// 				}
+	// };
+
 	var policyAttributes = {
 					"first":{
-								"1":"value1",
-								"2":"value2"
+								"1:Gold":"value1",
+								"1:Silver":"value2",
+								"2:Gold":"value3",
+								"2:Silver":"value4"
 					},
 					"second":{
-								"1":"true",
-								"2":"false"
+								"1:Gold":"true",
+								"1:Silver":"true",
+								"2:Gold":"false",
+								"2:Silver":"false"
 					},
 					"third":{
-								"1":"10",
-								"2":"20"						
+								"1:Gold":"10",
+								"1:Silver":"20",
+								"2:Gold":"30",
+								"2:Silver":"40"					
 					}
-			  };
+	};			  
 
 	var companyIds = new Array();
 
-	var results = JSON.parse('[{"idv_value":1290100,"company_id":1,"company_name":"ICICI","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":2,"company_name":"BAJAJ","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":3,"company_name":"TATA","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":4,"company_name":"RELIANCE","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38}]');
+//	var results = JSON.parse('[{"idv_value":1290100,"company_id":1,"company_name":"ICICI","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":2,"company_name":"BAJAJ","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":3,"company_name":"TATA","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":4,"company_name":"RELIANCE","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38}]');
+
+	var results = JSON.parse('[{"idv_value":1290100,"company_id":1,"plan":"Gold","company_name":"ICICI","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":2,"plan":"Gold","company_name":"BAJAJ","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":3,"plan":"Silver","company_name":"TATA","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38},{"idv_value":1290100,"company_id":4,"company_name":"RELIANCE","base_od":44380,"elec_acc":0,"non_elec_acc":0,"bi_fuel_od":0,"net_od":44380,"anti_theft_dis":0,"aai_dis":0,"ncb_dis":8876,"net_dis":8876,"final_od":35504,"base_tp":2853,"bi_fuel_tp":60,"owner_pa":100,"passenger_pa":0,"pad_driver":50,"final_tp":3063,"total_premium":38567,"service_tax":4767,"final_premium":43334,"motor_search_id":38}]');
+
 
 	//var results = window.quotes;
 
@@ -47,9 +72,14 @@ function __common__policyCompare(policyType){
 	html[++h] = "<td style='border:none;'></td>"; //We need one empty cell to store the attribute names
 	for(var result, i = -1; result = results[++i];){
 		html[++h] = "<td class='alert-success'>";
-		html[++h] = "<strong>"+result.company_name+"</strong>";
-		//TODO - add plan name as well
-		companyIds.push(result.company_id);
+		if(result.plan != null){
+			html[++h] = "<strong>"+result.company_name+"</strong>";
+			html[++h] = "<strong> - "+result.plan+"</strong>";
+			companyIds.push(result.company_id+":"+result.plan);
+		}else{
+			html[++h] = "<strong>"+result.company_name+"</strong>";
+			companyIds.push(result.company_id);
+		}
 		html[++h] = "</td>";
 	}
 	html[++h] = "</tr>";
