0b5804aa46b001760c977474a7a48103f3278213
Nikhil P Agrawal <nikhil.pa@gmail.com>
2013-03-23 21:03:07 +0530

    enabling disable of quote form

--- app/assets/javascripts/welcome/health/tooltips.js
+++ app/assets/javascripts/welcome/health/tooltips.js
@@ -1,5 +1,5 @@
 function __health__bindToolTips(){
-	$("[id=healthQuoteForm][id=health_cover_icon]").tooltip({"title":"Select health coverage amount.","placement":"right"});
-	$("[id=healthQuoteForm][id=no_of_childs_icon]").tooltip({"title":"More than 21 years old Child will not be covered.","placement":"right"});
-	$("[id=healthQuoteForm][id=adult_age_icon]").tooltip({"title":"Age of Elder spouse in the family.","placement":"right"});
+	$("[id=healthQuoteForm] [id=health_cover_icon]").tooltip({"title":"Select health coverage amount.","placement":"right"});
+	$("[id=healthQuoteForm] [id=no_of_childs_icon]").tooltip({"title":"More than 21 years old Child will not be covered.","placement":"right"});
+	$("[id=healthQuoteForm] [id=adult_age_icon]").tooltip({"title":"Age of Elder spouse in the family.","placement":"right"});
 }
