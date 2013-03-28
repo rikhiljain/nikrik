3885d05d6e5b3a290af9f9adb8e858dc9bc67f15
Rikhil Jain <rikhiljain@gmail.com>
2013-03-28 16:34:31 +0530

    added logo

--- db/schema/table/tb_policy_attributes.sql
+++ db/schema/table/tb_policy_attributes.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `policy_attributes` (
   `policy_type` varchar(20) NOT NULL,
   `plan` varchar(20) DEFAULT NULL,
   `attrib_name` varchar(255) NOT NULL,
-  `attrib_value` varchar(50) NOT NULL,
+  `attrib_value` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
