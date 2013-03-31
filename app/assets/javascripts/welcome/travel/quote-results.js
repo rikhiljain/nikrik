5be92ee414d48b006b515b08c2e1f84a881a222f
Rikhil Jain <rikhiljain@gmail.com>
2013-03-31 16:53:57 +0530

    update policy compare for motor and travel

--- app/assets/javascripts/welcome/travel/quote-results.js
+++ app/assets/javascripts/welcome/travel/quote-results.js
@@ -4,20 +4,29 @@ var results = data;
 window.quotes = data;
 var e = esc;
 var html = [], h = -1;
-html[++h] = "<table id='quoteTravelResultsTable' class='table table-bordered'>";
-html[++h] = "<th>Company Name</th>";
-html[++h] = "<th>Plan</th>";
-html[++h] = "<th>Final Premium</th>";
+html[++h] = "<p><button id='travelQuoteResultsRecomputeLink' class='btn is_btn-custom' type='button' onclick='__travel__recomputeQuoteForm()'>Recompute</button>";
+html[++h] = "&nbsp;&nbsp;"; 
+html[++h] = "<button id='motorQuoteResultsCompareLink' class='btn is_btn-custom' type='button' onclick='__travel__policyCompare(true)'>Compare All Policies</button></p>";
+html[++h] = "<table id='quoteTravelResultsTable' class='table table-striped'>";
+html[++h] = "<thead><tr><th style='text-align:center;vertical-align:middle;' >Company Name</th>";
+html[++h] = "<th style='text-align:center;vertical-align:middle;' >Plan</th>";
+html[++h] = "<th><button id='motorQuoteResultsCompareLink' class='btn is_btn-custom' type='button' onclick='__travel__policyCompare(false)'>Compare</button></th>";
+html[++h] = "<th style='text-align:center;vertical-align:middle;' >Final Premium</th>";
+html[++h] = "<th style='text-align:center;vertical-align:middle;'>Reward Points</th>";
 html[++h] = "<th></th>";
-html[++h] = "<tbody>";
+html[++h] = "</tr></thead><tbody>";
 for(var result, i = -1; result = results[++i];){
-	html[++h] = "<tr><td>";
-	html[++h] = result.company_name;
-	html[++h] = "</td><td>";
+	html[++h] = "<tr><td style='text-align:center;vertical-align:middle;' >";
+	html[++h] = "<img src='/assets/company/logo/logo_" + result.company_id + ".png' />";
+	html[++h] = "</td><td style='text-align:center;vertical-align:middle;' >";
 	html[++h] = result.plan;
-	html[++h] = "</td><td>";
+	html[++h] = "</td><td style='text-align:center;vertical-align:middle;' >";
+	html[++h] = "<input id='compare' type='checkbox' value='true' </input>";
+	html[++h] = "</td><td style='text-align:center;vertical-align:middle;'>";
 	html[++h] = "<span class='rupee'>` "+result.total_premium+"</span>";
-	html[++h] = "</td><td>";
+	html[++h] = "</td><td style='text-align:center;vertical-align:middle;'>";
+	html[++h] = result.points;
+	html[++h] = "</td><td style='text-align:center;vertical-align:middle;'>";
 	html[++h] = "<button class='btn is_btn-custom' type='button' onclick='__common__openQuoteBuyForm("+result.company_id+");'>Buy</a>";
 	html[++h] = "</td></tr>";
 }
@@ -25,7 +34,7 @@ html[++h] = "</tbody>";
 html[++h] = "</table>";
 html[++h] = "<button id='travelQuoteResultsRecomputeLink' class='btn is_btn-custom' type='button' onclick='__travel__recomputeQuoteForm()'>Recompute</button>";
 html[++h] = "&nbsp;&nbsp;"; 
-html[++h] = "<button id='motorQuoteResultsCompareLink' class='btn is_btn-custom' type='button' onclick='__common__policyCompare(\"Travel\")'>Compare All Policies</button>";
+html[++h] = "<button id='motorQuoteResultsCompareLink' class='btn is_btn-custom' type='button' onclick='__travel__policyCompare(true)'>Compare All Policies</button>";
 window.$quoteResultsAccordionTable[0].innerHTML = html.join('');
 }
 
--- app/models/travel/quote.rb
+++ app/models/travel/quote.rb
@@ -1,12 +1,13 @@
 include ActionView::Helpers::NumberHelper
 
 class Travel::Quote
-  attr_accessor :company_id, :company_name, :total_premium, :service_tax, :final_premium,:plan, :user_id, :travel_search_id
+  attr_accessor :company_id, :company_name, :total_premium, :service_tax, :final_premium,:plan, :points,:user_id, :travel_search_id
   	
 
   def self.format_fields (quote)
     quote.total_premium = ((quote.total_premium.nil?)? 0 : quote.total_premium.ceil )
     quote.final_premium = ((quote.final_premium.nil?)? 0 : quote.final_premium.ceil )
+    quote.points = ((quote.points.nil?)? 0 : quote.points.ceil )
   end
 
  end
