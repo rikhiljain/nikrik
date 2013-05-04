69d194f04203c6906a0318e0c0d140c95616e760
Rikhil Jain <rikhiljain@gmail.com>
2013-05-04 14:58:15 +0530

    health bug fix

--- app/assets/javascripts/welcome/health/static-data.js
+++ app/assets/javascripts/welcome/health/static-data.js
@@ -36,11 +36,18 @@ function __health__populateHealthCover( selectElement )
 {
 	var options = [
 					{"optionValue":"", "optionDisplay": "Select Amount"},
-	               	{"optionValue":"100000", "optionDisplay": "100000"},
-	               	{"optionValue":"200000", "optionDisplay": "200000"},
-    	          	{"optionValue":"300000", "optionDisplay": "300000"},
-	               	{"optionValue":"400000", "optionDisplay": "400000"},
-	               	{"optionValue":"500000", "optionDisplay": "500000"}
+	               	{"optionValue":"100000", "optionDisplay": "1 Lakh"},
+	               	{"optionValue":"200000", "optionDisplay": "2 Lakh"},
+    	          	{"optionValue":"300000", "optionDisplay": "3 Lakh"},
+	               	{"optionValue":"400000", "optionDisplay": "4 Lakh"},
+	               	{"optionValue":"500000", "optionDisplay": "5 Lakh"},
+	               	{"optionValue":"700000", "optionDisplay": "7 Lakh"},
+	               	{"optionValue":"750000", "optionDisplay": "7.5 Lakh"},
+	               	{"optionValue":"1000000", "optionDisplay": "10 Lakh"},
+	               	{"optionValue":"1500000", "optionDisplay": "15 Lakh"},
+	               	{"optionValue":"2000000", "optionDisplay": "20 Lakh"},
+	               	{"optionValue":"3000000", "optionDisplay": "30 Lakh"},
+	               	{"optionValue":"5000000", "optionDisplay": "50 Lakh"}
 	              ];
 	$.each(options, function(){
 		options += '<option value="' + this['optionValue'] + '">' + this['optionDisplay'] + '</option>';
